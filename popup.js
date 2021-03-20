dcf3eb5387e685e0fe13080c1b471d65b038da0e
msir3316 <owatadetteiu@yahoo.co.jp>
2021-03-20 22:45:47 +0900

    新機能
    ポップアップにテキストの任意入力欄を追加

--- popup.js
+++ popup.js
@@ -1,2 +1,9 @@
+function setFreeText(){
+    let t = document.getElementById("free-title").value;
+    let a = document.getElementById("free-artist").value;
+    setMusicInfo(({title: t, artist: a}));
+}
+
 document.getElementById("setText").addEventListener("click", getMusicInfo);
 document.getElementById("reset").addEventListener("click", resetText);
+document.getElementById("free-text").addEventListener("click", setFreeText);
