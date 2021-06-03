8ee95e33e947a7d90f0d90e219d6b675aa152964
msir3316 <owatadetteiu@yahoo.co.jp>
2021-06-03 22:11:49 +0900

    fix
    修正忘れ

--- popup.js
+++ popup.js
@@ -1,9 +1,7 @@
 function setFreeText(){
     let t = document.getElementById("free-title").value;
     let a = document.getElementById("free-artist").value;
-    chrome.storage.local.get(["serverOS"],function(items){
-        setMusicInfo( ({title: t, artist: a}), items.serverOS);
-    });
+    setMusicInfo( ({title: t, artist: a}));
     
 }
 
